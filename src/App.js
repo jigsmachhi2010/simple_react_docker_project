5ca78579d86a69cedc122c6eb94d7b2e0543d69c
jigswebbrains <jignesh@webbrainstechnologies.com>
2022-07-27 18:14:12 +0530

    Docker FIle

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome<code> TO </code>My WORLD.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.instagram.com/_.jigs._machhi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Self Jignesh Machhi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
